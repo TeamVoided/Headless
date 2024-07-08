25d3dff9fb08d3c14d1261cc70329a1686afe2e5
theendercore <theendercore@gmail.com>
2024-07-08 23:09:52 +0300

    - delete ur self now

--- src/client/kotlin/org/teamvoided/headless/HeadlessClient.kt
+++ src/client/kotlin/org/teamvoided/headless/HeadlessClient.kt
@@ -1,12 +1,9 @@
 package org.teamvoided.headless
 
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry
-import net.minecraft.client.MinecraftClient
 import net.minecraft.client.render.block.entity.SkullBlockEntityRenderer
 import net.minecraft.client.render.block.entity.model.SkullBlockEntityModel
 import net.minecraft.client.render.entity.model.EntityModelLayer
-import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import org.teamvoided.headless.Headless.log
 import org.teamvoided.headless.skull.CustomSkullType
@@ -16,20 +13,9 @@ object HeadlessClient {
     fun init() {
         log.info("Hello from Client")
         SkullBlockEntityRenderer.TEXTURES[CustomSkullType.HUSK] =
-            Identifier.ofDefault("textures/entity/zombie/husk.png")
+            Identifier.ofDefault("textures/entity/zombie/drowned.png")
         EntityModelLayerRegistry.registerModelLayer(HUSK_HEAD, SkullBlockEntityModel::getHeadTexturedModelData)
-
-
-        ClientTickEvents.END_WORLD_TICK.register {
-            var x = ""
-            DEBUG_VALUE.forEach { (a, b) -> x += "$a: [$b] |" }
-            MinecraftClient.getInstance().player?.sendMessage(Text.of(x), true)
-        }
     }
-
-
-    @JvmField
-    val DEBUG_VALUE = mutableMapOf<String, Any>()
 
     val HUSK_HEAD = registerMain("husk_head")
 
--- src/main/kotlin/org/teamvoided/headless/data/gen/HeadlessData.kt
+++ src/main/kotlin/org/teamvoided/headless/data/gen/HeadlessData.kt
@@ -21,27 +21,10 @@ class HeadlessData : DataGeneratorEntrypoint {
         val pack = gen.createPack()
 
 //        pack.addProvider(::TemplateWorldGenerator)
-
-        pack.addProvider { o: FabricDataOutput ->
-            object : FabricModelProvider(o) {
-                override fun generateBlockStateModels(gen: BlockStateModelGenerator) {
-                }
-
-                override fun generateItemModels(gen: ItemModelGenerator) {
-                    gen.skull(SkullInit.HUSK_SKULL_ITEM)
-//        gen.register(DuskItems.BOGGED_SKULL, parentedItemModel(mc("template_skull")))
-//        gen.register(DuskItems.GLOOM_SKULL, parentedItemModel(mc("template_skull")))
-                }
-            }
-        }
     }
 
     override fun buildRegistry(gen: RegistrySetBuilder) {
 //        gen.add(RegistryKeys.BIOME, TemplateBiomes::boostrap)
     }
 
-    fun ItemModelGenerator.skull(item: Item) =
-        this.register(item, parentedItemModel(Identifier.ofDefault("template_skull")))
-
-    fun parentedItemModel(id: Identifier) = Model(Optional.of(id.withPrefix("item/")), Optional.empty())
 }
