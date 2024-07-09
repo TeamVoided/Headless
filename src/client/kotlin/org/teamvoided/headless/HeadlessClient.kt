f008fe38c175194257933b837dee80fc3e3361ad
theendercore <theendercore@gmail.com>
2024-07-10 01:33:25 +0300

    - layered skulls

--- src/client/kotlin/org/teamvoided/headless/HeadlessClient.kt
+++ src/client/kotlin/org/teamvoided/headless/HeadlessClient.kt
@@ -10,29 +10,35 @@ import org.teamvoided.headless.skull.CustomSkullType
 
 @Suppress("unused")
 object HeadlessClient {
-    val BOGGED_HEAD = register("bogged_head", "main")
-    val bogged_png = Identifier.ofDefault("textures/entity/skeleton/bogged.png")
-
     val HUSK_HEAD = register("husk_head", "main")
     val husk_png = Identifier.ofDefault("textures/entity/zombie/husk.png")
 
+    val BOGGED_HEAD = register("bogged_head", "main")
+    val BOGGED_HEAD_OVERLAY = register("bogged_head", "overlay")
+    val bogged_png = Identifier.ofDefault("textures/entity/skeleton/bogged.png")
+    val bogged_png2 = Identifier.ofDefault("textures/entity/skeleton/bogged_overlay.png")
+
     val STRAY_HEAD = register("stray_head", "main")
+    val STRAY_HEAD_OVERLAY = register("stray_head", "overlay")
     val stray_png = Identifier.ofDefault("textures/entity/skeleton/stray.png")
+    val stray_png2 = Identifier.ofDefault("textures/entity/skeleton/stray_overlay.png")
 
     fun init() {
         log.info("Hello from Client")
-
-        EntityModelLayerRegistry.registerModelLayer(BOGGED_HEAD, SkullBlockEntityModel::getSkullTexturedModelData)
-        HeadlessRegistry.registerSkull(CustomSkullType.BOGGED, bogged_png)
-        { SkullBlockEntityModel(it.getModelPart(BOGGED_HEAD)) }
 
         EntityModelLayerRegistry.registerModelLayer(HUSK_HEAD, SkullBlockEntityModel::getHeadTexturedModelData)
         HeadlessRegistry.registerSkull(CustomSkullType.HUSK, husk_png)
         { SkullBlockEntityModel(it.getModelPart(HUSK_HEAD)) }
 
-        EntityModelLayerRegistry.registerModelLayer(STRAY_HEAD, SkullBlockEntityModel::getSkullTexturedModelData)
+        EntityModelLayerRegistry.registerModelLayer(BOGGED_HEAD, LayeredSkullModel::getSkullTexturedModelData)
+        EntityModelLayerRegistry.registerModelLayer(BOGGED_HEAD_OVERLAY, LayeredSkullModel::getSkullTexturedModelOverlayData)
+        HeadlessRegistry.registerSkull(CustomSkullType.BOGGED, bogged_png)
+        { LayeredSkullModel(it.getModelPart(BOGGED_HEAD), it.getModelPart(BOGGED_HEAD_OVERLAY), bogged_png2) }
+
+        EntityModelLayerRegistry.registerModelLayer(STRAY_HEAD, LayeredSkullModel::getSkullTexturedModelData)
+        EntityModelLayerRegistry.registerModelLayer(STRAY_HEAD_OVERLAY, LayeredSkullModel::getSkullTexturedModelOverlayData)
         HeadlessRegistry.registerSkull(CustomSkullType.STRAY, stray_png)
-        { SkullBlockEntityModel(it.getModelPart(STRAY_HEAD)) }
+        { LayeredSkullModel(it.getModelPart(STRAY_HEAD), it.getModelPart(STRAY_HEAD_OVERLAY), stray_png2) }
 
     }
 
---
+++ src/client/kotlin/org/teamvoided/headless/LayeredSkullModel.kt
@@ -0,0 +1,82 @@
+package org.teamvoided.headless
+
+import com.mojang.blaze3d.vertex.VertexConsumer
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.model.*
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.block.entity.model.SkullBlockEntityModel
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.util.Identifier
+
+open class LayeredSkullModel(root: ModelPart, hat: ModelPart,  layer: Identifier) : SkullBlockEntityModel(root) {
+    val OUTER_LAYER: RenderLayer = RenderLayer.getEntityCutoutNoCullZOffset(layer)
+    protected val hat: ModelPart = hat.getChild("hat")
+
+    override fun setHeadAngles(animationProgress: Float, yaw: Float, pitch: Float) {
+        super.setHeadAngles(animationProgress, yaw, pitch)
+        hat.yaw = yaw * 0.017453292f
+        hat.pitch = pitch * 0.017453292f
+    }
+
+    override fun method_2828(matrices: MatrixStack, vertexConsumer: VertexConsumer, i: Int, j: Int, k: Int) {
+        super.method_2828(matrices, vertexConsumer, i, j, k)
+
+        val provider = MinecraftClient.getInstance().bufferBuilders.entityVertexConsumers
+        val buffer = provider.getBuffer(OUTER_LAYER)
+        hat.method_22699(matrices, buffer, i, j, k)
+        provider.draw(OUTER_LAYER)
+    }
+
+    companion object {
+        fun makeHed(): ModelData {
+            val modelData = ModelData()
+            val modelPartData = modelData.root
+            modelPartData.addChild(
+                "head",
+                ModelPartBuilder.create()
+                    .uv(0, 0)
+                    .cuboid(-4.0f, -8.0f, -4.0f, 8.0f, 8.0f, 8.0f),
+                ModelTransform.NONE
+            )
+            modelPartData.addChild(
+                "hat",
+                ModelPartBuilder.create()
+                    .uv(0, 0)
+                    .cuboid(-4.0f, -8.0f, -4.0f, 8.0f, 8.0f, 8.0f, Dilation(0.25f)),
+                ModelTransform.NONE
+            )
+
+            return modelData
+        }
+
+        fun getHeadTexturedModelData(): TexturedModelData {
+            val meshdefinition = makeHed()
+            return TexturedModelData.of(meshdefinition, 64, 64)
+        }
+
+        fun getSkullTexturedModelData(): TexturedModelData {
+            val modelData = ModelData()
+            val modelPartData = modelData.root
+            modelPartData.addChild(
+                "head",
+                ModelPartBuilder.create()
+                    .uv(0, 0)
+                    .cuboid(-4.0f, -8.0f, -4.0f, 8.0f, 8.0f, 8.0f),
+                ModelTransform.NONE
+            )
+            return TexturedModelData.of(modelData, 64, 32)
+        }
+        fun getSkullTexturedModelOverlayData(): TexturedModelData {
+            val modelData = ModelData()
+            val modelPartData = modelData.root
+            modelPartData.addChild(
+                "hat",
+                ModelPartBuilder.create()
+                    .uv(0, 0)
+                    .cuboid(-4.0f, -8.0f, -4.0f, 8.0f, 8.0f, 8.0f, Dilation(0.25f)),
+                ModelTransform.NONE
+            )
+            return TexturedModelData.of(modelData, 64, 32)
+        }
+    }
+}
