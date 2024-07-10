532e11a613aa46d1e0926a0690a696167e7cccb6
theendercore <theendercore@gmail.com>
2024-07-10 21:17:32 +0300

    - removed junk from mixin

--- src/client/java/org/teamvoided/headless/mixin/SkullBlockEntityRendererMixin.java
+++ src/client/java/org/teamvoided/headless/mixin/SkullBlockEntityRendererMixin.java
@@ -6,39 +6,21 @@ import net.minecraft.block.SkullBlock;
 import net.minecraft.client.render.block.entity.SkullBlockEntityRenderer;
 import net.minecraft.client.render.block.entity.model.AbstractSkullBlockEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLoader;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Debug;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.teamvoided.headless.HeadlessRegistry;
 
 import java.util.Map;
 
-import static org.teamvoided.headless.Headless.log;
-
 @Debug(export = true)
 @Mixin(SkullBlockEntityRenderer.class)
 public abstract class SkullBlockEntityRendererMixin {
-
-    @Shadow @Final private static Map<SkullBlock.SkullType, Identifier> TEXTURES;
-
-    @Shadow @Final private Map<SkullBlock.SkullType, AbstractSkullBlockEntityModel> models;
-
     @Inject(at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;"), method = "getModels")
     private static void addCustomModels(EntityModelLoader modelLoader, CallbackInfoReturnable<Map<SkullBlock.SkullType, AbstractSkullBlockEntityModel>> cir,
                                         @Local ImmutableMap.Builder<SkullBlock.SkullType, AbstractSkullBlockEntityModel> builder) {
         HeadlessRegistry.getSkulls().forEach((type, model) -> builder.put(type, model.invoke(modelLoader)));
     }
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void x(CallbackInfo ci) {
-        this.models.forEach((type, model) -> log.info("type: {} model: {}", type, model));
-        TEXTURES.forEach((type, texture) -> log.info("type: {} texture: {}", type, texture));
-    }
-
 }
